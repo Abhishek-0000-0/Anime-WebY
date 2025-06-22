aa534bff636171118491e31e2195ff1c6ad994c4
Abhishek-0000-0 <linasura000@icloud.com>
2025-06-22 23:02:16 +0530

    Initial
    Create Backend + Frontend with all library downloaded and proper file - folder structure

---
+++ Backend/src/app.js
@@ -0,0 +1,29 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Welcome to Anime API",
+    endpoints: {
+      anime: "/api/anime",
+      docs: "/api-docs",
+    },
+  });
+});
+
+app.use((req, res) => {
+  res.status(404).json({ message: "Route not found" });
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ message: "Internal server error" });
+});
+
+export default app;
